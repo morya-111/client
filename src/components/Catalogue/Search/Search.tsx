8d441d6e0705f1928fcc2e2f06d6769f7be5398c
Atharva Bhange <atharva.bhange@gmail.com>
2021-10-28 16:36:26 +0530

    Drawer on small screen complete

--- src/components/Catalogue/Search/Search.tsx
+++ src/components/Catalogue/Search/Search.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import Drawer from "components/Drawer";
+import React, { useState } from "react";
 import { FaFilter } from "react-icons/fa";
+import Sidebar from "../Sidebar";
 
 const Search: React.FC = () => {
+	const [isDrawerOpen, setDrawerOpen] = useState(true);
+
 	return (
 		<div className="flex w-full">
 			<div className="mr-3 md:hidden">
-				<FaFilter size={30} className="text-dark" />
+				<FaFilter
+					size={30}
+					className="text-dark"
+					onClick={() => setDrawerOpen(true)}
+				/>
 			</div>
 			<div className="flex-grow text-lg text-center bg-gray-400">
 				Search Goes here
 			</div>
+			<Drawer isOpen={isDrawerOpen} onClose={() => setDrawerOpen(false)}>
+				<div className="h-full px-8 py-4 rounded-r-lg bg-light">
+					<Sidebar />
+				</div>
+			</Drawer>
 		</div>
 	);
 };
--- src/pages/Catalogue/Catalogue.tsx
+++ src/pages/Catalogue/Catalogue.tsx
@@ -6,7 +6,7 @@ const Catalogue: React.FC = () => {
 	return (
 		<div className="grid w-full h-screen grid-cols-12 gap-2 px-2 md:px-12 bg-light">
 			<div
-				style={{ borderWidth: "1px" }}
+				style={{ borderRightWidth: "1px" }}
 				className="hidden col-span-2 md:block xl:col-span-1 border-opacity-20 border-r-dark"
 			>
 				<Sidebar />
