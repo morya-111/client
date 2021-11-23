59c503eeee720247925a85548e40a69312dd3426
Invincible <prathameshsujgure33@gmail.com>
2021-11-24 01:05:07 +0530

    Homepage Stage3 (semiFinal)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,19 +4,25 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
 	return (
-		<div className="relative w-full px-4 py-2 mx-auto sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 bg-dark">
+		<div className="relative flex items-center w-full px-4 py-2 mx-auto space-y-0 sm:py-4 sm:space-x-6 bg-dark">
 			<LogoLight className="w-auto h-16" />
-			<div className="absolute flex flex-row items-center right-32 bottom-50 text-light space-x-14 sm:mx-0 sm:flex-shrink-0 ">
-				<NavLink className="motion-safe:hover:scale-110 " to="/about">
+			<div className="absolute flex flex-row items-center space-x-5 right-4 sm:right-9 md:right-32 md:bottom-50 text-light sm:space-x-14 sm:mx-0 sm:flex-shrink-0 ">
+				<NavLink
+					className="text-sm motion-safe:hover:scale-110 sm:text-base"
+					to="/about"
+				>
 					About
 				</NavLink>
 				<NavLink
-					className="motion-safe:hover:scale-110 "
+					className="text-sm motion-safe:hover:scale-110 sm:text-base "
 					to="/privacyPolicy"
 				>
 					Privacy Policy
 				</NavLink>
-				<NavLink className="motion-safe:hover:scale-110 " to="/contact">
+				<NavLink
+					className="text-sm motion-safe:hover:scale-110 sm:text-base"
+					to="/contact"
+				>
 					Contact
 				</NavLink>
 			</div>
--- src/components/Homepage/Landing/Landing.tsx
+++ src/components/Homepage/Landing/Landing.tsx
@@ -33,13 +33,17 @@ const Landing = () => {
 
 			<Dropdown isOpen={isOpen} />
 
-			<div className="relative w-auto h-screen bg-light">
-				<div className="absolute top-0 ml-5 md:mt-24 md:flex flex-nowrap">
-					<div>
-						<InfoText
-							title="Trying To Find Your Favorite book?"
-							subTitle="Browse large catalogue of books to rent or buy from people close to you!"
-						/>
+			<div className="container min-w-full min-h-screen overflow-hidden bg-light">
+				<div className="top-0 flex pt-10 mb-5 ml-7 md:ml-16 md:flex flex-nowrap">
+					<div className="">
+						<h1 className="text-4xl md:text-6xl text-dark">
+							Trying To Find Your Favorite book?
+						</h1>
+						<h2 className="mt-2 text-2xl md:text-3xl text-dark">
+							Browse large catalogue of books to rent or buy from
+							people close to you!
+						</h2>
+
 						<div className="grid grid-rows-2 gap-2 mt-5 place-content-start">
 							<button className="px-4 py-1 rounded bg-semiLight text-light">
 								<NavLink to="/goToCatalogue">
@@ -51,43 +55,54 @@ const Landing = () => {
 							</button>
 						</div>
 					</div>
-					<div className="md:flex-shrink-0">
-						<HeroImg className="w-auto h-48 md:h-72 xl:w-auto xl:h-full lg:h-96 lg:w-auto md:w-auto" />
+					<div>
+						<HeroImg className="flex flex-none flex-shrink-0 " />
 					</div>
 				</div>
 			</div>
-			<div className="relative w-auto h-screen bg-clip-padding bg-semiDark">
+			<div className="w-auto min-h-screen bg-clip-padding bg-semiDark">
 				<div className="flex justify-center">
-					<div className="absolute w-screen top-1/4">
+					<div className="w-screen mt-10 md:mt-36">
 						<h1 className="text-5xl text-center text-light ">
 							What Can I Do Here?
 						</h1>
-						<div className="justify-around md:flex md:flex-wrap mt-9">
-							<div>
+
+						<div className="grid justify-center mt-16 md:justify-around md:flex md:flex-wrap">
+							<div className="h-auto w-72">
 								<First className="h-auto w-72" />
-								<div>
-									<InfoText
-										title="Books Catalogue"
-										subTitle="Explore huge catalogue of new and used books you can rent or buy from locals near you!"
-									/>
+								<div className="mt-3">
+									<h1 className="text-lg text-light">
+										Books Catalogue
+									</h1>
+									<h2 className="text-base text-light">
+										Explore huge catalogue of new and used
+										books you can rent or buy from locals
+										near you!
+									</h2>
 								</div>
 							</div>
-							<div>
+							<div className="h-auto mt-8 md:mt-0 w-72">
 								<Second className="h-auto w-72" />
-								<div>
-									<InfoText
-										title="Books Catalogue"
-										subTitle="Explore huge catalogue of new and used books you can rent or buy from locals near you!"
-									/>
+								<div className="mt-3">
+									<h1 className="text-lg text-light">
+										Books Listing
+									</h1>
+									<h2 className="text-base text-light">
+										Are you a huge book worm? You can sell
+										or rent your books on our book listing.
+									</h2>
 								</div>
 							</div>
-							<div>
+							<div className="h-auto mt-8 mb-8 md:mt-0 w-72">
 								<Third className="h-auto w-72" />
-								<div>
-									<InfoText
-										title="Books Catalogue"
-										subTitle="Explore huge catalogue of new and used books you can rent or buy from locals near you!"
-									/>
+								<div className="mt-3">
+									<h1 className="text-lg text-light">
+										Join Community
+									</h1>
+									<h2 className="text-base text-light">
+										Join huge community of book geeks
+										sharing books and experience with other.
+									</h2>
 								</div>
 							</div>
 						</div>
