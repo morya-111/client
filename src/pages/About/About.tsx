caa083459f1aee2231eda1d7248b4d1784417494
Invincible <prathameshsujgure33@gmail.com>
2021-12-02 01:38:21 +0530

    About Page minor changes

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -34,7 +34,7 @@ const About = () => {
 										>
 											<path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
 										</svg>
-										<p className="text-lg font-bold leading-relaxed md:text-2xl">
+										<p className="text-lg font-bold leading-relaxed lg:text-2xl md:text-xl">
 											With reading, you have this
 											opportunity to learn whatever you
 											want from the greatest people who
@@ -65,14 +65,17 @@ const About = () => {
 							</section>
 						</div>
 					</div>
-					<div className="text-[13px] sm:hidden m-1 mt-96 pt-10 text-center">
-						<b className="text-semiDark">BookEx</b> is a community
-						made to connect readers and encourage them to
-						<br />{" "}
-						<b className="opacity-75 text-semiDark">
-							Read Everyday
-						</b>{" "}
-						without any limitations of finding books.
+					<div className="flex justify-center">
+						<div className="text-[13px] sm:hidden m-1 mt-96 pt-10 text-left">
+							<b className="text-semiDark">BookEx</b> is a
+							community made to connect readers and encourage them
+							to
+							<br />{" "}
+							<b className="opacity-75 text-semiDark">
+								Read Everyday
+							</b>{" "}
+							without any limitations of finding books.
+						</div>
 					</div>
 				</div>
 				<div className="mb-7">
