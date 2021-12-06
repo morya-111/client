6d2a178cac94e3892f22375631cc64eb31dd7092
Invincible <prathameshsujgure33@gmail.com>
2021-12-07 04:42:55 +0530

    SignUp button fixed in NavBar

--- src/components/GoToButtons/SignUp/SignUp.tsx
+++ src/components/GoToButtons/SignUp/SignUp.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 const SignUp = () => {
-	return (
+	return (<NavLink to="/signup">
 		<button className="items-center px-8 py-2 text-center border-2 rounded-full bg-semiLight text-light hover:text-dark hover:bg-light hover:border-2 hover:border-dark"> 
-			<NavLink to="/signup">Sign Up</NavLink>
-		</button>
+			Sign Up
+		</button></NavLink>
 	);
 };
 export default SignUp;
