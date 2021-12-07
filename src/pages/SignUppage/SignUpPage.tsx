e807333d48df584345d4517ac55997346001e23f
Invincible <prathameshsujgure33@gmail.com>
2021-12-07 19:04:10 +0530

    completed signIn signUp pages (added background and home button)

--- src/pages/SignUppage/SignUpPage.tsx
+++ src/pages/SignUppage/SignUpPage.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import SignUpForm from "components/SignUpForm";
 import "tailwindcss/tailwind.css";
+import { ReactComponent as LogoDark } from "assets/common/logo-main-dark.svg";
+import { NavLink } from "react-router-dom";
+
 const Authpage: React.FC = () => {
 	return (
-		<div className="flex content-center mt-12">
-			<SignUpForm />
-		</div>
+		<>
+			<div className="min-h-screen bg-light">
+				<div className="pt-3">
+					<div className="flex justify-center">
+						<NavLink to="/">
+							<LogoDark className="w-28" />
+						</NavLink>
+					</div>
+					<div className="flex content-center">
+						<SignUpForm />
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
